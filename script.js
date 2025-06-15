177d4ac5f894c1e73b8a92790807579afc48e550
Anil : gadheanil57@gmail.com <gadheanil57@gmail.com>
2025-06-15 10:23:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
+    });
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("animate__animated", "animate__fadeInUp");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+document.querySelectorAll('.festival-card').forEach(card => {
+    observer.observe(card);
+});
+
+
+var loader = document.querySelector("#loader");
+setTimeout(function () {
+    loader.style.top = "-100%"
+}, 14000)
